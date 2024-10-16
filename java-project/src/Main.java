365ef6f08dd06083d91693bde5094393220846c5
jracharya <jracharya123@gmail.com>
2024-10-16 22:28:01 +0530

    Update agin Main.java

--- java-project/src/Main.java
+++ java-project/src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.println("Edit one more file");
+        Student s = new Student();
+        s.section();
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
---
+++ java-project/src/Student.java
@@ -0,0 +1,11 @@
+public class Student {
+
+    public Student()
+    {
+        System.out.println("hello this a constructor");
+    }
+    public void section()
+    {
+        System.out.println("Hi, I am a student");
+    }
+}
